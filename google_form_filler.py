10b383549d7dbfe163749f7d701ac701091d7f51
tobigiwa <giwaoluwatobi@gmail.com>
2023-09-10 05:12:43 -0400

    edit of time lag

--- google_form_filler.py
+++ google_form_filler.py
@@ -3,17 +3,21 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 import random
-import sys
 from selenium.webdriver.common.by import By
 from dataclasses import dataclass
 from typing import NoReturn, List
 
-URL: str = "https://docs.google.com/forms/d/e/1FAIpQLSeG_hjjalfPzCFvS_pBT6FYnj5URYeMmCkIooOq0DbXA6LDXg/viewform"
+URL: str = "https://docs.google.com/forms/d/e/1FAIpQLSfXqX1d34P7SwUC7Wp2adN6pBGuLpv4DbSKywTXJfcDTJnYsQ/viewform"
+
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
 driver: webdriver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()))
+    service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 
 @dataclass
@@ -39,44 +43,46 @@ class Scrape:
             "HOW MANY TIMES WOULD YOU LIKE TO FILL THIS FORM, PRESS ENTER FOR DEFAULT(ONCE):")
         if runs == "" or runs is None:
             runs: int = 1
-            print("\nSince you went for default, robot will fill form once")
+            print("\nSince you went for default, Robot will fill form once")
         else:
             runs: int = int(runs)
-            print(f"\nRobot will fill form {runs +1 } times\n\n")
+            print(f"\nRobot will fill form {runs} times\n\n")
 
         for run in range(runs):
 
             print("\n*****************************************************")
-            print("                                                     ")
             print("AT RUN:", run + 1)
-            print("                                                     ")
-            print("*****************************************************\n")
+            print("*****************************************************")
 
             self.browser.get(url)
 
             allQuestions = self.FindAll(".geS5n")
-            print("Total lenght of questions is------", len(allQuestions))
+            # print("Total lenght of questions is------", len(allQuestions))
             counter: int = 0
 
             for eachQuestion in allQuestions:
-                print("question at number-----", counter + 1,
-                      "Question is ```  ", self.FindAll(".geS5n .M7eMe")[counter].text, "   ```")
+                # print("question at number-----", counter + 1,
+                #   "Question is ```  ", self.FindAll(".geS5n .M7eMe")[counter].text, "   ```")
 
                 allOptions: List[WebElement] = eachQuestion.find_elements(
                     By.CSS_SELECTOR, ".nWQGrd.zwllIb")
 
                 choice: WebElement = random.choice(allOptions)
-                print(
-                    f"this question has {len(allOptions)} options, I'll be picking option {allOptions.index(choice) + 1} which is   ``{choice.text}``  \n")
+                # print(
+                # f"this question has {len(allOptions)} options, I'll be picking option {allOptions.index(choice) + 1} which is   ``{choice.text}``  \n")
                 choice.click()
-                time.sleep(3)
+                time.sleep(0.4)
                 counter += 1
 
-            print("done filling form")
-            time.sleep(3)
+            # print("done filling form")
+            time.sleep(0.4)
             self.FindOne(".lRwqcd .NPEfkd").click()
-            print("form submitted...")
-            time.sleep(4)
+            # print("form submitted...")
+
+            print("*****************************************************")
+            print("RUN:", run + 1, "COMPLETED")
+            print("*****************************************************\n")
+            time.sleep(1.2)
 
         print("\n\n*****************************************************")
         print("BYE!!!...hoped Robot served you well")
